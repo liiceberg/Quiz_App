fcc2fe74dc79d9f46f73129e71844c28cc6bf0c3
liiceberg <2aysylu2003@gmail.com>
2024-05-14 13:56:28 +0300

    unused imports deleted

--- app/src/main/java/ru/kpfu/itis/gimaletdinova/quizapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/gimaletdinova/quizapp/presentation/MainActivity.kt
@@ -2,23 +2,20 @@ package ru.kpfu.itis.gimaletdinova.quizapp.presentation
 
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
 import androidx.datastore.DataStore
 import androidx.datastore.preferences.Preferences
 import androidx.datastore.preferences.edit
-import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import ru.kpfu.itis.gimaletdinova.quizapp.R
-import ru.kpfu.itis.gimaletdinova.quizapp.presentation.game.QuestionViewModel
-import ru.kpfu.itis.gimaletdinova.quizapp.util.setCurrentTheme
 import ru.kpfu.itis.gimaletdinova.quizapp.util.PrefsKeys.NIGHT_MODE_KEY
 import ru.kpfu.itis.gimaletdinova.quizapp.util.PrefsKeys.THEME_CHANGED_KEY
+import ru.kpfu.itis.gimaletdinova.quizapp.util.setCurrentTheme
 import javax.inject.Inject
 
 @AndroidEntryPoint
